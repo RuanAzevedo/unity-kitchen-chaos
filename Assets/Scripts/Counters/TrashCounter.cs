4362c17b4a0f84e0a3da451c1c782170cf0135ab
Ruan Azevedo <ruan.azevedo@gmail.com>
2023-06-17 12:19:30 -0300

    🔊 Add SFXs

--- Assets/Scripts/Counters/TrashCounter.cs
+++ Assets/Scripts/Counters/TrashCounter.cs
@@ -1,12 +1,15 @@
-using UnityEngine;
+using System;
 
 public class TrashCounter : BaseCounter
 {
+    public static event EventHandler OnObjectTrashed;
+
     public override void Interact(Player player)
     {
         if (player.HasKitchenObject())
         {
             player.GetKitchenObject().DestroySelf();
+            OnObjectTrashed?.Invoke(this, EventArgs.Empty);
         }
     }
 
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,75 @@
+using System;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance { get; private set; }
+
+    [SerializeField] private AudioClipRefsSO audioClipRefsSO;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        DeliveryManager.Instance.OnRecipeSuccess += DeliveryManager_OnRecipeSucess;
+        DeliveryManager.Instance.OnRecipeFailed += DeliveryManager_OnRecipeFailed;
+        CuttingCounter.OnAnyCut += CuttingCounter_OnAnyCut;
+        Player.Instance.OnPickedSomething += Player_OnPickedSomething;
+        BaseCounter.OnAnyObjectPlacedHere += BaseCounter_OnAnyObjectPlacedHere;
+        TrashCounter.OnObjectTrashed += TrashCounter_OnObjectTrashed;
+    }
+
+    private void TrashCounter_OnObjectTrashed(object sender, EventArgs e)
+    {
+        TrashCounter trashCounter = sender as TrashCounter;
+        PlaySound(audioClipRefsSO.trash, base.transform.position);
+    }
+
+    private void BaseCounter_OnAnyObjectPlacedHere(object sender, EventArgs e)
+    {
+        BaseCounter baseCounter = sender as BaseCounter;
+        PlaySound(audioClipRefsSO.objectDrop, base.transform.position);
+    }
+
+    private void Player_OnPickedSomething(object sender, EventArgs e)
+    {
+        PlaySound(audioClipRefsSO.objectPickup, Player.Instance.transform.position);
+    }
+
+    private void CuttingCounter_OnAnyCut(object sender, EventArgs e)
+    {
+        CuttingCounter cuttingCounter = sender as CuttingCounter;
+        PlaySound(audioClipRefsSO.chop, cuttingCounter.transform.position);
+    }
+
+    private void DeliveryManager_OnRecipeFailed(object sender, EventArgs e)
+    {
+        DeliveryCounter deliveryCounter = DeliveryCounter.Instance;
+        PlaySound(audioClipRefsSO.deliveryFail, deliveryCounter.transform.position);
+    }
+
+    private void DeliveryManager_OnRecipeSucess(object sender, EventArgs e)
+    {
+        DeliveryCounter deliveryCounter = DeliveryCounter.Instance;
+        PlaySound(audioClipRefsSO.deliverySuccess, deliveryCounter.transform.position);
+    }
+
+    private void PlaySound(AudioClip[] audioClipArray, Vector3 position, float volume = 1f)
+    {
+        PlaySound(audioClipArray[UnityEngine.Random.Range(0, audioClipArray.Length)], position, volume);
+    }
+
+    private void PlaySound(AudioClip clip, Vector3 position, float volume = 1f)
+    {
+        AudioSource.PlayClipAtPoint(clip, position, volume);
+    }
+
+    public void PlayFootstepsSound(Vector3 position, float volume)
+    {
+        PlaySound(audioClipRefsSO.footstep, position, volume);
+    }
+
+}
