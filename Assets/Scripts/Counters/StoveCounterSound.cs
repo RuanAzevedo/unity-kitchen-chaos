834237bf18746578fea1f68bc700b377f8f50779
Ruan Azevedo <ruan.azevedo@gmail.com>
2023-06-22 16:16:59 -0300

    💄 Add animations & sfx to the stove progress bar

--- Assets/Scripts/Counters/StoveCounterSound.cs
+++ Assets/Scripts/Counters/StoveCounterSound.cs
@@ -6,7 +6,26 @@ using UnityEngine;
 public class StoveCounterSound : MonoBehaviour
 {
     [SerializeField] private StoveCounter stoveCounter;
+
     private AudioSource audioSource;
+    private float warningSoundTimer;
+    private bool playWarningSound;
+
+    private void Update()
+    {
+        if (playWarningSound)
+        {
+            warningSoundTimer -= Time.deltaTime;
+
+            if (warningSoundTimer <= 0f)
+            {
+                float warningSoundTimerMax = .2f;
+                warningSoundTimer = warningSoundTimerMax;
+
+                SoundManager.Instance.PlayWarningSound(stoveCounter.transform.position);
+            }
+        }
+    }
 
     private void Awake()
     {
@@ -16,6 +35,13 @@ public class StoveCounterSound : MonoBehaviour
     private void Start()
     {
         stoveCounter.OnStateChanged += StoveCounter_OnCounterStateChanged;
+        stoveCounter.OnProgressChanged += StoveCounter_OnProgressChanged;
+    }
+
+    private void StoveCounter_OnProgressChanged(object sender, IHasProgress.OnProgressChangedEventArgs e)
+    {
+        float burnShowProgressAmount = .5f;
+        playWarningSound = stoveCounter.IsFried() && e.progressNormalized >= burnShowProgressAmount;
     }
 
     private void StoveCounter_OnCounterStateChanged(object sender, StoveCounter.OnStateChangedEventArgs e)
